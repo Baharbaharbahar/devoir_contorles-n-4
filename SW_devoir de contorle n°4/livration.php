7beef0ed306e76c52ff744efc99a516320bd513f
hi I'm bahaeddine <119966574+Baharbaharbahar@users.noreply.github.com>
2024-02-25 23:34:39 +0100

    Update livration.php

--- SW_devoir de contorle n°4/livration.php
+++ SW_devoir de contorle n°4/livration.php
@@ -6,17 +6,17 @@ $conn = mysqli_connect($server, $username, $password, $bd) or die("Probleme de c
 $sql = "select distance from distance ;";
 
 echo $sql;
-// $res = mysqli_query($conn, $sql);
-// if ($poids < 1) {
-//     $poids = 8;
-// } else if ($poids >= 1) {
-//     $poids = 0.001;
-// } else if (!isset($_POST["livraison"])) {
-//     $livraison = "Oui";
-// } else {
-//     $sql1 = "insert into distance values ('$date','$expediteur','$destinatatire','$de','$vers','$poids','$livraison','$tel');";
-//     $res1 = mysqli_query($conn, $sql1);
-//     if (mysqli_affected_rows($conn) > 0) {
-//         echo "livraison enregistree avec succes";
-//     }
-// }
+ $res = mysqli_query($conn, $sql);
+ if ($poids < 1) {
+     $poids = 8;
+ } else if ($poids >= 1) {
+     $poids = 0.001;
+ } else if (!isset($_POST["livraison"])) {
+     $livraison = "Oui";
+ } else {
+     $sql1 = "insert into distance values ('$date','$expediteur','$destinatatire','$de','$vers','$poids','$livraison','$tel');";
+     $res1 = mysqli_query($conn, $sql1);
+    if (mysqli_affected_rows($conn) > 0) {
+        echo "livraison enregistree avec succes";
+     }
+ }
