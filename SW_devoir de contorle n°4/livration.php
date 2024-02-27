c8a112aaca4797e229faf86d2a38010e8f3b999b
hi I'm bahaeddine <119966574+Baharbaharbahar@users.noreply.github.com>
2024-02-27 23:08:02 +0100

    Update livration.php

--- SW_devoir de contorle n°4/livration.php
+++ SW_devoir de contorle n°4/livration.php
@@ -1,22 +1,29 @@
 <?php
-$poids = $_POST["poids"];
+$date=$_POST["date"];
+$destinatatire=$_POST["destinatatire"];
+$de=$_POST["de"];
+$vers=$_POST["vers"];
+$poids=$_POST["poids"];
+$livraison=$_POST["livraison"];
+$tel=$_POST["tel"];
+$poids=$_POST['poids'];
 $livraison = $_POST["livraison"];
 require("config.php");
 $conn = mysqli_connect($server, $username, $password, $bd) or die("Probleme de connxion au serveur ou à la base de données");
-$sql = "select distance from distance ;";
-
-echo $sql;
- $res = mysqli_query($conn, $sql);
- if ($poids < 1) {
-     $poids = 8;
- } else if ($poids >= 1) {
-     $poids = 0.001;
- } else if (!isset($_POST["livraison"])) {
-     $livraison = "Oui";
- } else {
-     $sql1 = "insert into distance values ('$date','$expediteur','$destinatatire','$de','$vers','$poids','$livraison','$tel');";
-     $res1 = mysqli_query($conn, $sql1);
+$sql = "select * from livraison ; ";
+$res = mysqli_query($conn,$sql);
+$nb=mysqli_num_rows($res);
+echo $nb;
+if ($poids < 1) {
+    $prix = 8;
+} else if ($poids > 1) {
+    $prix =$prix +($prix*0.001);
+} else if (isset($vers)){
+    $prix=$prix*1.5;
+} else {
+    $sql1 = "insert into livrison values ('$date','$expediteur','$destinatatire','$de','$vers','$poids','$livraison','$tel','$prix');";
+    $res1 = mysqli_query($conn, $sql1);
     if (mysqli_affected_rows($conn) > 0) {
         echo "livraison enregistree avec succes";
-     }
- }
+    }
+}
